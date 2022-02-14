8720d2b064ec0b1ee4773885234e83f759525a73
Ayushma Sharma <ayushmasharma63@gmail.com>
2022-02-14 23:49:54 +0530

    Firebase Connected

--- app/src/main/java/com/example/recruitz/activities/MainActivity.kt
+++ app/src/main/java/com/example/recruitz/activities/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.recruitz
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
